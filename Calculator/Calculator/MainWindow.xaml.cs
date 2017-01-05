e4b0e7701ba568bea831f671a676f96d57c15207
OlgaV <oleovic@rambler.ru>
2017-01-05 16:39:15 +0500

    add Add and Sub

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -20,9 +20,40 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly ICalculator _calculator;
+        private string  arg1, arg2;
+
         public MainWindow()
         {
             InitializeComponent();
+            _calculator = new Calc();
+            Argument1TextBox.Clear();
+            Argument1TextBox.Focus();
+            Argument2TextBox.Clear();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var ax = int.Parse(arg1);
+            var ay = int.Parse(arg2);
+            ResulTextBlock.Text = _calculator.Add(ax, ay).ToString();
+        }
+
+        private void Argument2TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            arg2 = Argument2TextBox.Text;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var ax = int.Parse(arg1);
+            var ay = int.Parse(arg2);
+            ResulTextBlock.Text = _calculator.Sub(ax, ay).ToString();
+        }
+
+        private void Argument1TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            arg1 = Argument1TextBox.Text;
         }
     }
 }
