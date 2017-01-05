1149d92b304e201301c27d406030f28111345238
OlgaV <oleovic@rambler.ru>
2017-01-05 16:05:46 +0500

    errorr

--- Calculator/Calculator/Calc.cs
+++ Calculator/Calculator/Calc.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Calc
+    class Calc : ICalculator
     {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Div(int a, int b)
+        {
+            if (b != 0) return a / b;
+            else throw new DivideByZeroException();
+           
+        }
+
+        public int Mod(int a, int b)
+        {
+            if (b != 0) return a % b;
+            else throw new DivideByZeroException();
+        }
+
+        public int Mult(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
     }
 }
